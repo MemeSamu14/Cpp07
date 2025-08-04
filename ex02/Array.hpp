c4dff8854142edbc86360b5b22ceb1f9fb1a082e
Samuele Fiorini <sfiorini@c2r4p8.42firenze.it>
2025-08-04 12:25:33 +0200

    works all perfectly, i need to fix the heap allocation with the operator << followed by the element

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: sfiorini <sfiorini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 15:04:38 by sfiorini          #+#    #+#             */
-/*   Updated: 2025/08/03 17:09:12 by sfiorini         ###   ########.fr       */
+/*   Updated: 2025/08/04 12:24:56 by sfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 #include <iostream>
 #include <stdexcept>
 
+int	abs(int num)
+{
+	if (num < 0)
+		num = num * (-1);
+	return (num);
+}
+
 template <typename T>
 class Array
 {
@@ -24,7 +31,13 @@ class Array
 		unsigned int	s;
 	public:
 		Array(){this->array = new T[0]; this->s = 0;};
-		Array(unsigned int n){ this->array = new T[n]; this->s = n;};
+		Array(unsigned int n)
+		{
+			this->array = new T[n];
+			for (int i = 0; i < static_cast<int>(n); i++)
+				this->array[i] = 0;
+			this->s = n;
+		};
 		Array(const Array &a): array(a.array){};
 		const Array& operator=(const Array &a)
 		{
@@ -33,22 +46,21 @@ class Array
 			for (int i = 0; a.array[i]; i++)
 				this->array[i] = a.array[i];
 		};
-		unsigned int size() const { return (this->s); };
+		int	size() const { return (static_cast<int>(this->s)); };
 		~Array(){ delete[] array; };
 		T&	operator[](int idx)
 		{
-			try
-			{
-				if (idx >= static_cast<int>(this->s))
-					throw std::out_of_range("Out of Range");
-			}
-			catch (const std::out_of_range& e)
-			{
-				std::cout << e.what() << std::endl;
-			}
+			if (abs(idx) >= static_cast<int>(this->s) || idx < 0)
+				throw std::out_of_range("Out of Range");
 			return (this->array[idx]);
 		};
 };
 
+// template <typename T>
+// std::ostream& operator<<(std::ostream& os, const T& a)
+// {
+// 	os << a << std::endl;
+// 	return os;
+// }
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sfiorini <sfiorini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 14:10:44 by sfiorini          #+#    #+#             */
-/*   Updated: 2025/08/03 17:08:17 by sfiorini         ###   ########.fr       */
+/*   Updated: 2025/08/04 12:25:03 by sfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,34 @@ int	main( void )
 {
 	Array<int> a(10);
 
-	// a[-1] = 5;
-	a[0] = 5;
-	a[1] = 5;
-	std::cout << a[0] << std::endl;
+	try
+	{
+		a[10] = 5; // too high index
+	}
+	catch (const std::out_of_range& e){ std::cout << e.what() << std::endl; }
+	try
+	{
+		a[-2] = 5; // too low index
+	}
+	catch (const std::out_of_range& e){ std::cout << e.what() << std::endl; }
+	try
+	{
+		a[0] = 50; // correct index
+		std::cout << a[0] << std::endl;
+	}
+	catch (const std::out_of_range& e){ std::cout << e.what() << std::endl; }
+	try
+	{
+		for (int i = 0; i < a.size(); i++)
+			a[i] = i;
+		for (int i = 0; i < a.size(); i++)
+			std::cout << a[i] << std::endl;
+	}
+	catch (const std::out_of_range& e){ std::cout << e.what() << std::endl; }	
+	
+	Array<int> *b = new Array<int>(5);
+	// std::cout << b[0] << std::endl;
+	delete b;
+
 	return (0);
 }
